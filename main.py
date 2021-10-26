23408ccf64ab782e898930a5a675e5f608072a03
Marco Gregori <marco.gregori74@gmail.com>
2021-10-26 18:13:39 -0300

    Implementación Problema 1 (Regex)

--- main.py
+++ main.py
@@ -8,6 +8,37 @@
 
 '''         FUNCIONES              '''
 
+
+def ProblemaRegex():
+    print('''
+    Problema 1 : Las matrículas de las aeronaves en Argentina tienen el siguiente formato de acuerdo a su tipo (donde
+    abc significa 3 letras mayúsculas y 123 3 dígitos):
+
+    Solución: "L[VQ]-[A-Z]\w{2,4}"
+
+''')
+    eleccion = input("Desea ver el siguiente problema [1] o volver al menu [0]?:")
+    if eleccion == "1":
+        print('''
+        Problema 2 : Diseñe una ER para validar cadenas de números naturales menores a 1900.
+
+        Solucion: "^([1-9][0-9]{0,2}|[1][0-8][0-9][0-9]|[-][0-9]*)$" }
+        ''')
+        eleccion2 = input("Desea ver el ejercicio anterior [1] o volver al menu [0]?:")
+        if eleccion2 == "1":
+            ProblemaRegex()
+        elif eleccion2 == "0":
+            ShowMenu()
+        else:
+            print("Error")
+
+    elif eleccion == "0":
+        ShowMenu()
+    else:
+        print("Error ")
+
+
+
 def ShowMenu():
     res = True
     while (res == True):
@@ -24,7 +55,7 @@ def ShowMenu():
                 ''')
         eleccion = input("Ingrese los problemas que desees ver... ")
         if eleccion == '1':
-            print("a")
+            ProblemaRegex()
         elif eleccion == '2':
             print("a")
         elif eleccion == '3':
@@ -35,7 +66,7 @@ def ShowMenu():
             print("Adios")
             break
         else:
-            print("Ingrese un parametro   valido")
+            print("Ingrese un parametro valido")
             break
 
         return None
